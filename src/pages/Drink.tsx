0c899993701a966fa7eb30b89b7066a97840123b
Lachicagladiadora <200379@unsaac.edu.pe>
2024-07-23 22:58:18 -0500

    chore: add types and fix Drink component

--- src/pages/Drink.tsx
+++ src/pages/Drink.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { DrinkData, DrinkType } from "../types";
 
 type DrinkProps = { drinkName: string };
 
@@ -6,16 +7,16 @@ const getDrink = async (name: string) => {
   const newDrink = await fetch(
     `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`
   );
-  const drink = await newDrink.json();
+  const drink: DrinkData = await newDrink.json();
   console.log(drink["drinks"][0]);
   return drink["drinks"][0];
 };
 
 export const Drink = ({ drinkName = "margarita" }: DrinkProps) => {
-  const [recipe, setRecipe] = useState("");
+  const [recipe, setRecipe] = useState<DrinkType | null>(null);
 
   const drinkRecipe = async () => {
-    const data = await getDrink(drinkName);
+    const data: DrinkType = await getDrink(drinkName);
     console.log({ data });
     // const drinkRecipe = data;
     setRecipe(data);
@@ -26,20 +27,42 @@ export const Drink = ({ drinkName = "margarita" }: DrinkProps) => {
   });
 
   return (
-    <div>
-      <header>
-        <h2>
-          I will drink <span>{recipe["strDrink"]}</span>
-        </h2>
-      </header>
-      <section>
-        <h3>ingredients</h3>
-        <ul>{recipe}</ul>
-      </section>
-      <section>
-        <h3>instructions</h3>
-        <p>{recipe["strInstructions"]}</p>
-      </section>
-    </div>
+    <>
+      {!recipe && (
+        <p>
+          <span className="capitalize">{drinkName}</span> not found
+        </p>
+      )}
+      {recipe && (
+        <>
+          <header>
+            <h2>
+              I will drink <span>{recipe.strDrink}</span>
+            </h2>
+          </header>
+          <section>
+            <h3>ingredients</h3>
+            <ul>
+              <li>
+                {recipe.strIngredient1} <span>{recipe.strMeasure1}</span>
+              </li>
+              <li>
+                {recipe.strIngredient2} <span>{recipe.strMeasure2}</span>
+              </li>
+              <li>
+                {recipe.strIngredient3} <span>{recipe.strMeasure3}</span>
+              </li>
+              <li>
+                {recipe.strIngredient4} <span>{recipe.strMeasure4}</span>
+              </li>
+            </ul>
+          </section>
+          <section>
+            <h3>instructions</h3>
+            <p>{recipe["strInstructions"]}</p>
+          </section>
+        </>
+      )}
+    </>
   );
 };

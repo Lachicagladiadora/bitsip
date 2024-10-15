e45167352ac273e4dd01ccfef6936b97bc0bd94d
Lachicagladiadora <dev.roseny@gmail.com>
2024-10-15 18:35:27 -0500

    refactor: start refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,14 @@
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { ErrorPage } from "./ErrorPage";
-// import { WantMealOrDrink } from "./components/WantMealOrDrink";
-// import { useState } from "react";
-import { Home } from "./components/Home";
-import {
-  Routes,
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Home />,
-//     errorElement: <ErrorPage />,
-//     children: [
-//       {
-//         path: "/:option",
-//         element: <WantMealOrDrink option={`${option}`} />,
-//         // action: optionAction,
-//       },
-//       {
-//         path: "/:option",
-//         element: <WantMealOrDrink option="drink" />,
-//       },
-//     ],
-//   },
-// ]);
-
-const router = createBrowserRouter(createRoutesFromElements());
+import { BeakerIcon } from "@heroicons/react/16/solid";
 
 function App() {
-  // const [option, setOption] = useState<"meal" | "drink" | null>(null);
   return (
     <>
-      <main className="w-full flex-1 flex flex-col items-center justify-center">
-        {/* <RouterProvider router={router} /> */}
-        {/* <Routes>
-          <Route path="/" element={<Home />} errorElement={<ErrorPage />}> */}
-        {/* <Route
-              path={`/${option}`}
-              element={<WantMealOrDrink option={option} />}
-            /> */}
-        {/* </Route>
-        </Routes> */}
-        {/* <Route path="/:mealOrDrink"></Route> */}
-        {/* </Route> */}
-        {/* <Outlet /> */}
-      </main>
-      <footer className="h-8 text-center">by Lachicagladiadora</footer>
+      <div>
+        App
+        <BeakerIcon className="size-6 text-blue-500" />
+      </div>
     </>
   );
 }
 
-// option={option} setOption={setOption}
 export default App;

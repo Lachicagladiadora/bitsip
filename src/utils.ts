7add2901948ff0360d6c1ef7758ccebb12e52c32
Lachicagladiadora <dev.roseny@gmail.com>
2024-11-12 23:57:01 -0500

    chore: add try catch to pure functions in utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { DrinkData, DrinkType, MealData, MealType } from "./types";
+import { DrinkType, MealData, MealType } from "./types";
 
 // pure functions
 
@@ -6,53 +6,73 @@ import { DrinkData, DrinkType, MealData, MealType } from "./types";
 
 // random
 export const getRandomMeal = async () => {
-  const random = await fetch(
-    "https://www.themealdb.com/api/json/v1/1/random.php"
-  );
+  try {
+    const random = await fetch(
+      "https://www.themealdb.com/api/json/v1/1/random.php"
+    );
 
-  const randomMeal = await random.json();
-  const data = randomMeal["meals"][0];
-  console.log({ randomMeal: data });
+    const randomMeal = await random.json();
+    const data = randomMeal["meals"][0];
+    console.log({ randomMeal: data });
 
-  return data;
+    return data;
+  } catch (error) {
+    console.error({ error });
+  }
 };
 
 export const getRandomDrink = async () => {
-  const random = await fetch(
-    "https://www.thecocktaildb.com/api/json/v1/1/random.php"
-  );
-  const randomDrink = await random.json();
-  const data = randomDrink["drinks"][0];
-  console.log({ randomDrink: data });
+  try {
+    const random = await fetch(
+      "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+    );
+    const randomDrink = await random.json();
+    const data = randomDrink["drinks"][0];
+    console.log({ randomDrink: data });
 
-  return data;
+    return data;
+  } catch (error) {
+    console.error({ error });
+  }
 };
 
 // By name
 export const getMeal = async (mealName: string): Promise<MealData> => {
-  const meal = await fetch(
-    `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`
-  );
-  const newMeal = await meal.json();
-  console.log({ newMeal });
-  return newMeal;
+  try {
+    const meal = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`
+    );
+    const newMeal = await meal.json();
+    console.log({ newMeal });
+    return newMeal;
+  } catch (error) {
+    console.error({ error });
+  }
 };
 
 export const getDrink = async (drinkName: string) => {
-  const drink = await fetch(
-    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkName}`
-  );
-  const newDrink = await drink.json();
-  console.log(newDrink);
+  try {
+    const drink = await fetch(
+      `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkName}`
+    );
+    const newDrink = await drink.json();
+    console.log(newDrink);
+  } catch (error) {
+    console.error({ error });
+  }
 };
 
 // By first letter
 export const getMealByFirstLetter = async (letter: string) => {
-  const response = await fetch(
-    `https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}`
-  );
-  const data = response.json();
-  return data;
+  try {
+    const response = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}`
+    );
+    const data = response.json();
+    return data;
+  } catch (error) {
+    console.error({ error });
+  }
 };
 
 // get a array according number in the final to property object
@@ -63,12 +83,16 @@ type GetStringListInput = {
 };
 
 export const getStringList = ({ object, entry }: GetStringListInput) => {
-  if (!object || !entry) return;
-  const list: string[] = [];
-  for (let index = 1; index < 20; index++) {
-    const elementList: string = object[entry + String(index)];
-    if (!elementList) return list;
-    list.push(elementList);
+  try {
+    if (!object || !entry) return;
+    const list: string[] = [];
+    for (let index = 1; index < 20; index++) {
+      const elementList: string = object[entry + String(index)];
+      if (!elementList) return list;
+      list.push(elementList);
+    }
+  } catch (error) {
+    console.error({ error });
   }
 };
 
@@ -76,10 +100,14 @@ export const getStringList = ({ object, entry }: GetStringListInput) => {
 type GetNameList = { objectList: MealType[] | DrinkType[]; entryName: string };
 
 export const getNameList = ({ objectList, entryName }: GetNameList) => {
-  const newList: string[] = [];
-  objectList.forEach((c) => {
-    const name: string = c[`${entryName}`];
-    newList.push(name);
-  });
-  return newList;
+  try {
+    const newList: string[] = [];
+    objectList.forEach((c) => {
+      const name: string = c[`${entryName}`];
+      newList.push(name);
+    });
+    return newList;
+  } catch (error) {
+    console.error({ error });
+  }
 };

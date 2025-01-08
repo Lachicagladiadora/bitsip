6f595dfe0ecb10d221a6e3038675c8d93f434db1
Lachicagladiadora <dev.roseny@gmail.com>
2025-01-07 19:48:16 -0500

    fix: styles in meal page

--- src/components/Meal.tsx
+++ src/components/Meal.tsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import { Ingredient, MealType } from "../types";
 import { RESPONSE_MEAL } from "../constants";
 import { getIngredientsFromMeal, getMealByName } from "../utils";
-import { PhotoIcon } from "@heroicons/react/24/outline";
+import { CheckIcon, PhotoIcon } from "@heroicons/react/24/outline";
 
 export const Meal = () => {
   const [currentMeal, setCurrentMeal] = useState<MealType | null>(null);
@@ -41,18 +41,19 @@ export const Meal = () => {
   }, [currentMeal]);
 
   return (
-    <div className="w-full h-full max-w-[800px] p-4 flex flex-col items-center justify-center gap-6">
+    <div className="w-dvw max-w-[800px] min-w-[300px] p-4 flex flex-col items-center justify-center gap-6">
       {!currentMeal && !isLoading && <p>Not found {meal} </p>}
       {!currentMeal && isLoading && <p>Loading {meal} ...</p>}
       {currentMeal && !isLoading && (
         <>
-          <header className="w-full flex gap-4 items-center justify-center md:justify-between md:gap-none">
+          <header className="w-full flex flex-col gap-4 items-center justify-center sm:flex-row md:justify-between md:gap-none overflow-hidden">
             {/* Image */}
-            <div className="relative size-48 sm:w-1/2 md:h-[320px] bg-center bg-cover rounded-lg shadow-md overflow-hidden">
+            <div className="relative flex-1 bg-center ">
               {currentMeal.strMealThumb ? (
                 <img
                   src={currentMeal.strMealThumb}
                   alt={currentMeal.strMeal ?? "Meal image"}
+                  className="object-cover rounded-lg shadow-md"
                 />
               ) : (
                 <PhotoIcon className="h-full w-full" />
@@ -62,37 +63,43 @@ export const Meal = () => {
               </div>
             </div>
             {/* Title */}
-            <h2 className="flex-1 font-WindSong text-xl text-center font-bold md:text-2xl lg:text-3xl">
+            <h2 className="font-WindSong text-xl text-center font-bold sm:w-52 md:w-80 md:text-2xl lg:text-3xl">
               {currentMeal.strMeal}
             </h2>
           </header>
           <main className="w-full flex flex-col gap-4">
             {/* Ingredients */}
             {ingredientsList.length > 0 && (
-              <section className="w-full flex">
-                <div className="flex-1">
-                  <h3 className="font-bold text-lg">Ingredients</h3>
-                  <ol className="list-inside list-disc">
+              <table className="border-none">
+                <thead>
+                  <tr>
+                    <th className="font-bold text-lg text-left">Ingredients</th>
+                    <th className="font-bold text-lg text-left">Quantities</th>
+                    {/* <th>Year</th> */}
+                  </tr>
+                </thead>
+                <tbody>
+                  <td>
                     {ingredientsList.map((c, i) => (
-                      <li key={i}>{c.strIngredient}</li>
+                      <tr key={i} className="flex items-center gap-2">
+                        <CheckIcon className="h-4 w-4" />
+                        {c.strIngredient}
+                      </tr>
                     ))}
-                  </ol>
-                </div>
-                <div className="w-1/2 md:w-1/3">
-                  <h3 className="font-bold text-lg">Quantities</h3>
-                  <ul>
+                  </td>
+                  <td>
                     {ingredientsList.map((c, i) => (
-                      <li key={i}>{c.strMeasure}</li>
+                      <tr key={i}>{c.strMeasure}</tr>
                     ))}
-                  </ul>
-                </div>
-              </section>
+                  </td>
+                </tbody>
+              </table>
             )}
             {/* Instructions */}
             <section>
               <h3 className="font-bold text-lg">Instructions</h3>
-              <p className="font-light indent-10 whitespace-pre-line">
-                {currentMeal?.strInstructions}
+              <p className="font-light whitespace-pre-line">
+                {currentMeal.strInstructions}
               </p>
             </section>
           </main>

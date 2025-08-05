ef6d1480d71f47859bc33ef2283f1a89f4463465
Lachicagladiadora <dev.roseny@gmail.com>
2025-08-04 23:51:24 -0500

    fix: header

--- src/outer/atomic/molecules/Header.tsx
+++ src/outer/atomic/molecules/Header.tsx
@@ -6,21 +6,23 @@ export const Header = () => {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
 
   return (
-    <div className="w-full p-4 flex items-center justify-between">
-      <h1 className="flex-1 font-WindSong font-bold text-center text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
-        <a href="/">BitSip</a>
-      </h1>
-      <button
-        className="size-6 sm:size-7 md:size-8 lg:size-9"
-        onClick={() => {
-          setIsDarkTheme((prev) => !prev);
-          const bodyElement = document.querySelector("html");
-          if (!bodyElement) return;
-          bodyElement.classList.toggle("dark");
-        }}
-      >
-        {isDarkTheme ? <SunIcon /> : <MoonIcon />}
-      </button>
+    <div className="w-full flex items-center justify-center">
+      <div className="w-full max-w-[900px] p-4 flex items-center justify-between">
+        <h1 className="flex-1 font-WindSong font-bold text-center text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
+          <a href="/">BitSip</a>
+        </h1>
+        <button
+          className="size-6 sm:size-7 md:size-8 lg:size-9"
+          onClick={() => {
+            setIsDarkTheme((prev) => !prev);
+            const bodyElement = document.querySelector("html");
+            if (!bodyElement) return;
+            bodyElement.classList.toggle("dark");
+          }}
+        >
+          {isDarkTheme ? <SunIcon /> : <MoonIcon />}
+        </button>
+      </div>
     </div>
   );
 };
